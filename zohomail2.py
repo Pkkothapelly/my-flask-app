db7f241ca654080ead8c2de2142fb21ad6958916
Pkkothapelly <164361628+Pkkothapelly@users.noreply.github.com>
2024-07-01 23:43:17 +0100

    Update zohomail2.py

--- zohomail2.py
+++ zohomail2.py
@@ -1505,4 +1505,5 @@ def send_policy_email():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(debug=True, host='0.0.0.0', port=port)
